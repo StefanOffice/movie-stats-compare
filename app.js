a7f554acc64055ae5dd9bd2e8de44a2c55df2ec6
Stefan Stefanovic <stefan.stefanovic.office@gmail.com>
2021-10-02 14:27:20 +0200

    Add dropdown search

--- app.js
+++ app.js
@@ -10,13 +10,32 @@ const fetchData = async (url, searchTerm) => {
     }
 
     const response = await axios.get(url, params);
-    console.log(response.data);
+    //the way this omdb api is designed is that it returns code 200 even if no results have been found but it sends an Error property in that case, so its necessary to check if it's present 
+    if(response.data.Error){
+        return [];
+    }
+
+    return response.data.Search;
 }
 
 const omdbUrl = 'http://www.omdbapi.com/';
 
 
-const onInput = (event) => { fetchData(omdbUrl, event.target.value); }
+const onInput = async (event) => { 
+    const movies = await fetchData(omdbUrl, event.target.value);
+    console.log(movies);
+
+    const container = document.querySelector("#resultList");
+    for(let movie of movies){
+        const result = document.createElement('li');
+        
+        result.innerHTML = `<a class="dropdown-item" href="#"><img src="${movie.Poster}" width="50px" height="50px"></img>${movie.Title}</a>`;
+
+        container.append(result);
+
+    }
+
+}
 
 const input = document.querySelector('input');
 input.addEventListener('input', createLimitedCallFunction(onInput));
